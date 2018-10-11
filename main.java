0d88c2ad32cce8baab129a1e44e712dde27bc7e1
meisasisit <43629087+meisasisit@users.noreply.github.com>
2018-10-10 21:26:06 -0600

    Add files via upload

---
+++ main.java
@@ -0,0 +1,13 @@
+
+public class main {
+	public static void main(String[] args) {
+	 	byte a = 0;
+	    short b = 1;
+	    int c = 3110;
+	    char d = 'w';
+	    boolean e = true;
+	    String output= "H" + c + " " + d + a + "rld 2.0 " + e;
+	        System.out.println(output);
+	        
+	    }
+}
